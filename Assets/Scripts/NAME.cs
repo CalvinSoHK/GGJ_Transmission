7f22b4a0368fa759cd063e9de3d8213a35f5d27f
Aleksei Waddington <amw694@nyu.edu>
2018-01-28 00:32:46 -0500

    Finalizing Text setup. needs to be tested

---
+++ Assets/Scripts/NAME.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NAME {
+
+    public static string name = "my dude";
+
+    public static void SetPlayerName(string Name)
+    {
+        name = Name;
+    }
+
+    public static string GetPlayerName()
+    {
+        return name;
+    }
+}
